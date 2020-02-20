98b9159a5b7681209c9b9ade60c119b03776c829
VICTOR SANCHEZ BARBERAN <vsanchba43.alumnes@ub052229.mat.ub.edu>
2020-02-20 10:02:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/vsanchba43alumnes/lab2/Activity2.kt
@@ -0,0 +1,12 @@
+package com.example.vsanchba43alumnes.lab2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+    }
+}
